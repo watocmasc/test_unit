ab55ab6a76abe1079a56fb63a0dedea553fa5cef
Watoc Masc X <oushen.m4@gmail.com>
2025-04-04 18:27:22 +0200

    install npm, init npm-jest, testing of function

--- math.test.js
+++ math.test.js
@@ -2,6 +2,6 @@ const { returnAddComponents } = require ( './math' );
 
 test ( 'adds 2 + 3 to equal 5', () => {
 
-    expect ( add( 2, 3 )).toBe (5);
-    
+    expect ( returnAddComponents ( 2, 3 ) ).toBe (10);
+
 } );

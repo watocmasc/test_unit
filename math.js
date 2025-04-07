f6f935aee1a97d530fb929a4fc7922020f6f4b09
Watoc Masc X <oushen.m4@gmail.com>
2025-04-07 18:26:13 +0200

    created funcs multi, subtract, factorial

--- math.js
+++ math.js
@@ -3,7 +3,27 @@ function returnAddComponents ( a, b ) {
 }
 
 function returnDivideComponents ( a, b ) {
+    if ( b === 0 ) throw new Error ( "Divisioin by zero" );
     return a / b;
 }
 
-module.exports = { returnAddComponents, returnDivideComponents };
+function returnMultiplyComponents ( a, b ) {
+    return a * b;
+}
+
+function returnSubtractComponents ( a, b ) {
+    return a - b;
+}
+
+function returnFactorial ( n ) {
+    if ( n < 0 ) throw new Error ( "Negative number" );
+    return n <= 1 ? 1 : n * returnFactorial ( n - 1 );
+}
+
+module.exports = { 
+    returnAddComponents, 
+    returnDivideComponents,
+    returnMultiplyComponents,
+    returnSubtractComponents,
+    returnFactorial
+ };

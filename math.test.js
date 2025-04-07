822bf2421fda0957d67dde963734e11f686083db
Watoc Masc X <oushen.m4@gmail.com>
2025-04-07 18:00:27 +0200

    created test of check function of divide

--- math.test.js
+++ math.test.js
@@ -1,7 +1,13 @@
-const { returnAddComponents } = require ( './math' );
+const { returnAddComponents, returnDivideComponents } = require ( './math' );
 
 test ( 'adds 2 + 3 to equal 5', () => {
 
-    expect ( returnAddComponents ( 2, 3 ) ).toBe (10);
+    expect ( returnAddComponents ( 2, 3 ) ).toBe (5);
+
+} );
+
+test ( 'divide 10 / 5 to equal 2', () => {
+
+    expect ( returnDivideComponents ( 10, 5 )).toBe (2) ;
 
 } );

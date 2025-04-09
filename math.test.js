0234fa34a253fbe5e2325e01cd6b38bb5ed7f2c3
Watoc Masc X <oushen.m4@gmail.com>
2025-04-09 18:12:20 +0200

    new feature

--- math.test.js
+++ math.test.js
@@ -1,12 +1,4 @@
-const { 
-
-    returnAddComponents, 
-    returnDivideComponents,
-    returnMultiplyComponents,
-    returnSubtractComponents,
-    returnFactorial
-
- } = require ( './math' );
+const { returnAddComponents, returnDivideComponents } = require ( './math' );
 
 test ( 'adds 2 + 3 to equal 5', () => {
 
@@ -18,22 +10,4 @@ test ( 'divide 10 / 5 to equal 2', () => {
 
     expect ( returnDivideComponents ( 10, 5 )).toBe (2) ;
 
-} );
-
-test ( 'multiply 7 * 6 to equal 42', () => {
-
-    expect ( returnMultiplyComponents ( 7, 6 )).toBe ( 42 );
-
-} );
-
-test ( 'subtract 94 - 11 to equal 83', () => {
-
-    expect ( returnSubtractComponents ( 94, 11 )).toBe ( 83 );
-
-} );
-
-test ( 'computes factorial correctly', () => {
-
-    expect ( returnFactorial ( 5 ) ).toBe ( 120 );
-
-} );// add comment
+} );//test comment

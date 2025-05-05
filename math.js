62a4f11948026e8741a815c8ef912c785bfa519b
watocmasc_win <oushen.m4@gmail.com>
2025-05-05 15:36:25 +0200

    add components

--- math.js
+++ math.js
@@ -2,8 +2,4 @@ function returnAddComponents ( a, b ) {
     return a + b;
 }
 
-function returnDivideComponents ( a, b ) {
-    return a / b;
-}
-
 module.exports = { returnAddComponents, returnDivideComponents };
